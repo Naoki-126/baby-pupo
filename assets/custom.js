b1e9fa129f26ec0f9a7c44af4747adb3d2cde444
Naoking <naokiyokota.126@gmail.com>
2025-10-08 08:03:49 +0900

    faqページ作成

--- assets/custom.js
+++ assets/custom.js
@@ -30,3 +30,33 @@ const swiper = new Swiper(".swiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+/* FAQページ アコーディオン
+=========================== */
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('#js-faq .accordion-a').forEach(content => {
+    content.style.maxHeight = '0px';
+    content.style.paddingTop = '0';
+    content.style.paddingBottom = '0';
+  });
+
+  document.querySelectorAll('#js-faq .accordion-q').forEach(trigger => {
+    const content = trigger.nextElementSibling;
+
+    trigger.addEventListener('click', function () {
+      const isActive = trigger.classList.contains('active');
+
+      if (isActive) {
+        trigger.classList.remove('active');
+        content.style.maxHeight = '0px';
+        content.style.paddingTop = '0';
+        content.style.paddingBottom = '0';
+      } else {
+        trigger.classList.add('active');
+        content.style.paddingTop = '14px';
+        content.style.paddingBottom = '14px';
+        content.style.maxHeight = (content.scrollHeight + 30) + 'px';
+      }
+    });
+  });
+});
