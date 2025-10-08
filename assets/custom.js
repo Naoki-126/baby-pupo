4b2b838bc50ebb3c87120cae91dbb6c022411aaa
Naoking <naokiyokota.126@gmail.com>
2025-10-09 08:30:30 +0900

    ブランドページ作成

--- assets/custom.js
+++ assets/custom.js
@@ -1,36 +1,5 @@
 // alert("テスト");
 
-/* ドロワー
-=========================== */
-const drawerBtn = document.getElementById("js-drawer");
-const drawerContents = document.getElementById("js-drawer__contents")
-
-drawerBtn.addEventListener("click",function (e) {
-  e.preventDefault();
-  drawerBtn.classList.toggle("is-checked");
-  drawerContents.classList.toggle("is-checked");
-});
-
-/* スワイパー
-=========================== */
-const swiper = new Swiper(".swiper", {
-    loop: true,
-    effect: "fade",
-    speed: 1000,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
 /* FAQページ アコーディオン
 =========================== */
 document.addEventListener('DOMContentLoaded', () => {
@@ -59,4 +28,35 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+});
+
+/* スワイパー
+=========================== */
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    effect: "fade",
+    speed: 1000,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+/* ドロワー
+=========================== */
+const drawerBtn = document.getElementById("js-drawer");
+const drawerContents = document.getElementById("js-drawer__contents")
+
+drawerBtn.addEventListener("click",function (e) {
+  e.preventDefault();
+  drawerBtn.classList.toggle("is-checked");
+  drawerContents.classList.toggle("is-checked");
 });
